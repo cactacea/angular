d60e7b25f4f53ff0e6722bed9a064dc6ec00674d
TAKESHI SHIMADA <>
2019-11-02 20:11:46 +0900

    v19.11.0 released

--- api.module.ts
+++ api.module.ts
@@ -3,42 +3,44 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Configuration } from './configuration';
 
-import { AccountsService } from './api/accounts.service';
-import { BlocksService } from './api/blocks.service';
+import { ChannelsService } from './api/channels.service';
 import { CommentLikesService } from './api/commentLikes.service';
 import { CommentsService } from './api/comments.service';
-import { FeedLikesService } from './api/feedLikes.service';
-import { FeedsService } from './api/feeds.service';
 import { FriendRequestsService } from './api/friendRequests.service';
-import { GroupsService } from './api/groups.service';
 import { InvitationsService } from './api/invitations.service';
 import { MediumsService } from './api/mediums.service';
 import { MessagesService } from './api/messages.service';
+import { NotificationsService } from './api/notifications.service';
+import { PasswordService } from './api/password.service';
 import { SessionService } from './api/session.service';
 import { SessionsService } from './api/sessions.service';
 import { SettingsService } from './api/settings.service';
 import { SystemService } from './api/system.service';
+import { TweetLikesService } from './api/tweetLikes.service';
+import { TweetsService } from './api/tweets.service';
+import { UsersService } from './api/users.service';
 
 @NgModule({
   imports:      [ CommonModule, HttpClientModule ],
   declarations: [],
   exports:      [],
   providers: [
-    AccountsService,
-    BlocksService,
+    ChannelsService,
     CommentLikesService,
     CommentsService,
-    FeedLikesService,
-    FeedsService,
     FriendRequestsService,
-    GroupsService,
     InvitationsService,
     MediumsService,
     MessagesService,
+    NotificationsService,
+    PasswordService,
     SessionService,
     SessionsService,
     SettingsService,
-    SystemService ]
+    SystemService,
+    TweetLikesService,
+    TweetsService,
+    UsersService ]
 })
 export class ApiModule {
     public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders {
